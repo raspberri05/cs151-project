985d1a7be419db3b3b2a3f54cd7ab21dad661fc3
Naya Singhania <naya.singhania@gmail.com>
2024-12-05 21:52:24 -0800

    add MenuManager get function

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,2 +1,18 @@
+import java.util.List;
+
 public class MenuManager {
+    private cafe DB;
+	private List<MenuItem> menu;
+
+    public MenuManager() {
+        DB = cafe.getInstance();
+        menu = DB.getMenu();
+    }
+
+    public void addMenuItem(MenuItem item) {
+        DB.addMenuItem(item);
+    }
+    public List<MenuItem> getMenu() {
+        return DB.getMenu();
+    }
 }
--- src/cafe.java
+++ src/cafe.java
@@ -18,7 +18,7 @@ public enum cafe {
 		menu = new ArrayList<>();
 		users = new HashMap<>();
 		loadUsersFromFile("resources/cafeData.txt");
-
+		loadMenuFromFile("resources/cafeData.txt");
 	}
 
 	public static cafe getInstance() {
@@ -36,6 +36,11 @@ public enum cafe {
 	public void addUser(User user) {
 		users.put(user.getUserName(), user);
 		updateUsersFile();
+	}
+
+	public void addMenuItem(MenuItem item) {
+		menu.add(item);
+		updateMenuFile();
 	}
 
 	public void updateUser(User user) {
@@ -68,6 +73,24 @@ public enum cafe {
 		}
     }
 
+	private void loadMenuFromFile(String filename) {
+		menu.clear();
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line;
+			boolean isMenuSection = false;
+
+			while ((line = br.readLine()) != null) {
+				if (line.equals("Menu:")) {
+					isMenuSection = true;
+				} else if (isMenuSection && !line.isEmpty()) {
+					parseMenuItem(line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	private void parseUsers(String line) {
 		String[] tokens = line.split(";");
 		String role = tokens[0];
@@ -89,6 +112,26 @@ public enum cafe {
 			Customer customer = new Customer(firstName, lastName, email, userName, password, isActive, orderedItems);
 			users.put(userName, customer);
 		}
+	}
+
+	private void parseMenuItem(String line) {
+		String[] tokens = line.split(";");
+		String menuType = tokens[0];
+		String title = tokens[1];
+		String itemID = tokens[2];
+		String description = tokens[3];
+		float price = Float.parseFloat(tokens[4]);
+		int count = Integer.parseInt(tokens[5]);
+		boolean isAvailable = Boolean.parseBoolean(tokens[6]);
+		if (menuType.equals("Diner")) {
+			DinerMenuItem diner = new DinerMenuItem(title, itemID, description, price, count, isAvailable, false);
+			menu.add(diner);
+		}
+		else {
+			PancakeMenuItem pancake = new PancakeMenuItem(title, itemID, description, price, count, isAvailable, false);
+			menu.add(pancake);
+		}
+
 	}
 
 	private void updateUsersFile() {
@@ -118,6 +161,39 @@ public enum cafe {
 			bw.write("Users:\n");
 			for (User user : users.values()) {
 				bw.write(user.toDataString() + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void updateMenuFile() {
+		String filename = "resources/cafeData.txt";
+		List<String> fileContent = new ArrayList<>();
+		boolean isMenuSection = false;
+	
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (line.equals("Menu:")) {
+					isMenuSection = true;
+				} else if (isMenuSection && line.isEmpty()) {
+					isMenuSection = false;
+				} else if (!isMenuSection) {
+					fileContent.add(line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+			for (String line : fileContent) {
+				bw.write(line + "\n");
+			}
+			bw.write("Menu:\n");
+			for (MenuItem item : menu) {
+				bw.write(item.toDataString() + "\n");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
